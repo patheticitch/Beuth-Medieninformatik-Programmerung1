6d1b9d4ef226115599b82a336b29936851ee4e91
patheticitch <MichaelsMBP@Michaels-MacBook-Pro-2.local>
2016-10-26 22:54:57 +0200

    Ein paar Optimierungen

--- ERSTE_METHODEN/src/MoreMethods.java
+++ ERSTE_METHODEN/src/MoreMethods.java
@@ -5,8 +5,8 @@ public class MoreMethods {
 		PrintWriter out= new PrintWriter(System.out,true);
 		printAlphabet(out,false);
 		printAlphabet(out,true);
-		printCharSeries(out,'F',11);
-		printCharSeries(out,'a',22);
+		//printCharSeries(out,'F',11);
+		printCharSeries(out,'a',14);
 		
 				
 
@@ -35,9 +35,9 @@ public class MoreMethods {
 		}
 	public static String createCharSeries(char startChar, int anzahl){
 		String series="";//empty string to store a series of characters
-		int count=1; //counting the strings starts at one
+		int count=0; //counting the strings starts at one
 		for (char i=startChar;i<startChar+anzahl;i++) {	
-			if((count%10==0 && count !=0) || count==anzahl) {	//puts a new line every 10 characters and at the end ignoring the first character
+			if(((count+1)%10==0 && count!=0)  || count==anzahl) {	//puts a new line every 10 characters and at the end ignoring the first character
 				series+=i; //appends the current character to a string
 				series+="\n"; //appends a new line to the string 
 				count++;	//increases the counter by 1
@@ -49,7 +49,7 @@ public class MoreMethods {
 		} return series;
 	}
 	public static void printCharSeries(PrintWriter out, char startChar, int anzahl) {
-		out.println(createCharSeries(startChar, anzahl));
+		printText(out,createCharSeries(startChar, anzahl));
 	}
 	
 }
